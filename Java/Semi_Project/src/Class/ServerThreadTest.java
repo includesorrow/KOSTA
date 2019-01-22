1cfb1e7776ca948552b118916af7f8c1c7261ddc
includesorrow <includesorrow@gmail.com>
2019-01-22 14:41:39 +0900

    서버 쓰레드 테스트중

---
+++ Java/Semi_Project/src/Class/ServerThreadTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Class;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.Socket;
+
+/**
+ *
+ * @author KOSTA
+ */
+public class ServerThreadTest {
+    public void ServerThreadTest() throws Exception{
+     Socket s = new Socket("127.0.0.1", 7777);
+        String filename = null;
+  BufferedOutputStream toServer = null;
+  BufferedInputStream bis = null;
+  FileInputStream fis = null;
+  DataOutputStream dos = null;
+
+  try{
+
+   filename = "사공석준.txt";
+
+   toServer = new BufferedOutputStream( s.getOutputStream() );
+   dos = new DataOutputStream( s.getOutputStream() );
+   dos.writeUTF( filename );
+
+   fis = new FileInputStream( filename );
+   bis = new BufferedInputStream( fis );
+
+            int ch = 0;
+   //? file내용 유무 check하면서 읽어 들임과 동시에 server단으로 전송
+   
+            while((ch = bis.read()  ) != -1) {
+                toServer.write(ch);        
+   }    
+   
+            toServer.flush();     
+   toServer.close();
+            fis.close();
+            s.close();      
+   
+
+  }catch(FileNotFoundException fnfe){
+   fnfe.printStackTrace();
+  }catch(IOException ioe){
+   ioe.printStackTrace();
+  }      
+ }}
+
