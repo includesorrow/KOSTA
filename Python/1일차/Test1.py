b20de82ebb6d295a31b0130d8573ece152bbef9f
includesorrow <includesorrow@gmail.com>
2019-04-11 22:02:32 +0900

    파이썬_플라스크 2일차

--- Python/1일차/Test1.py
+++
@@ -1,6 +0,0 @@
-
-num_student = 5
-if num_student > 4:
-    print(True)
-else:
-    print(False)
--- Python/2일차/스파이더/Ex1_ReservedWord.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Apr  3 09:35:08 2019
-
-@author: daily
-"""
-
