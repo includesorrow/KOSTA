aeab2f1b6cbd9514df7f6506bb8212f336bff5f8
includesorrow <includesorrow@gmail.com>
2019-06-24 13:49:12 +0900

    Update Document

--- Python/Exam/새 폴더/PythonExam/PythonExam/PythonExam.py
+++ Python/Exam/새 폴더/PythonExam/PythonExam/PythonExam.py
@@ -8,8 +8,34 @@ f'나의 이름은 {name}입니다. 나이는 {age}입니다.'
 age = 30
 f'나는 내년이면 {age+1}살이 된다.'
 
-d = {'name':'홍길동', 'age':30}
-f'나의 이름은 {d["name"]}입니다. 나이는 {d["age"]}입니다.'
+d = {'name':'홍길동ㅋ','age':50}
+
+f'나의 이름은 {d["name"]}이다. 나이는 {d["age"]}이다.'
+
+
+a = "hoooobby"
+a.find('b')
+a.count("o")
+
+
+aa = "Life is too short"
+aa.index("t")
+
+#위치 알려주기는 find랑 index가 존재한다.
+
+aa.find("o")
+aa.index("o")
+
+
+
+# 문자열 삽입 join
+",".join('abcd')
+
+a = "Hi"
+a.lower()
+
+
+
 
 
 
